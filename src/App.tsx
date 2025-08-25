c6b4ed7a42fb2988b6971cb750e6474fa247998e
Kaianvs <kaianvs12@gmail.com>
2025-08-25 16:29:47 -0300

    feat: adicionando component container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/theme.css"
 import "./styles/global.css"
-import { Heading } from "./components/Heading"
+import { Container} from "./components/Container"
+
 
 export function App(){
     return(
-        <div>
-            <Heading>Olá mundo</Heading>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi iure porro ea fugiat nemo rerum doloribus necessitatibus ipsum ratione? Quis rem magnam incidunt! Accusamus eaque natus, ad soluta reiciendis atque!</p>
-        </div>
+        <>
+            <Container>
+                <section>LOGO</section>
+            </Container>
+        </>
     )
 }
