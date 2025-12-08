b833bbc72f46f6b6d4f2fc44462a572ccd0f04b9
Kaian Vasconcelos <kaianvs12@gmail.com>
2025-12-08 10:11:11 -0300

    feat: organizando os componentes em pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./styles/theme.css"
 import "./styles/global.css"
 import { Container} from "./components/Container"
-
+import { Heading } from "./components/Heading"
 
 export function App(){
     return(
         <>
+            <Container>
+                 <Heading>
+                    Chronos Pomodoro
+                </Heading>
+            </Container>
+           
             <Container>
                 <section>LOGO</section>
             </Container>
