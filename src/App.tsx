e99082c8ab152372fbac9c6ecbe54e638aba8ca5
Kaian Vasconcelos <kaianvs12@gmail.com>
2025-12-09 20:18:24 -0300

    feat: Criando logo com icone e link

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,17 @@ import "./styles/theme.css"
 import "./styles/global.css"
 import { Container} from "./components/Container"
 import { Heading } from "./components/Heading"
+import { Logo } from "./components/Logo"
 
 export function App(){
     return(
         <>
             <Container>
-                 <Heading>
-                    Chronos Pomodoro
-                </Heading>
+                 <Logo/>
             </Container>
            
             <Container>
-                <section>LOGO</section>
+                <Heading>MENU</Heading>
             </Container>
         </>
     )
