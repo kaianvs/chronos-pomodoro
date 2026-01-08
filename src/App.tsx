1270954c0d8ba8fd5fb3e28158121017596c3554
Kaian Vasconcelos <kaianvs12@gmail.com>
2026-01-08 18:04:17 -0300

    feat: add template e organizando as paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,14 @@
 import "./styles/theme.css"
 import "./styles/global.css"
-import { Container} from "./components/Container"
-import { Logo } from "./components/Logo"
-import { Menu } from "./components/Menu"
-import { Countdown } from "./components/Countdown"
-import { DefaultInput } from "./components/DefaultInput"
-import { Cycles } from "./components/Cycles"
-import { DefaultButton } from "./components/DefaultButton"
-import { PlayCircleIcon } from "lucide-react"
-import { Footer } from "./components/Footer"
+
+import { Home } from "./pages/Home"
+
 
 
 export function App(){
 
 
     return(
-        <>
-        
-            <Container>
-                 <Logo/>
-            </Container>
-
-            <Container>
-                <Menu/>
-            </Container>
-            <Container>
-                <Countdown/>
-            </Container>
-
-            <Container>
-                <form className="form" action= ''>
-                    <div className="formRow">
-                        <DefaultInput id = 'meuInput'  type = "text" labelText = "Task" placeholder = "Digite algo"/>
-
-                    </div>
-
-                    <div className="formRow">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-
-                    <div className="formRow">
-                       <Cycles/>
-                    </div>
-
-                    <div className="formRow">
-                        <DefaultButton>
-                            <PlayCircleIcon/>
-                        </DefaultButton>
-                    </div>
-                </form>
-            </Container>
-
-             <Container>
-                <Footer/>
-            </Container>
-        </>
+        <Home/>
     )
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,29 @@
+import { DefaultButton } from "../DefaultButton";
+import { DefaultInput } from "../DefaultInput";
+import { Cycles } from "../Cycles";
+import { PlayCircleIcon } from "lucide-react";
+
+export function MainForm(){
+  return(
+      <form className="form" action= ''>
+                    <div className="formRow">
+                        <DefaultInput id = 'meuInput'  type = "text" labelText = "Task" placeholder = "Digite algo"/>
+
+                    </div>
+
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formRow">
+                       <Cycles/>
+                    </div>
+
+                    <div className="formRow">
+                        <DefaultButton>
+                            <PlayCircleIcon/>
+                        </DefaultButton>
+                    </div>
+                </form>
+  )
+}
