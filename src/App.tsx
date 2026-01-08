b69d426e41c8723f2034039a40d79f30184c3940
Kaian Vasconcelos <kaianvs12@gmail.com>
2026-01-08 16:39:26 -0300

    feat: adicionando modo ligth

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export function App(){
 
     return(
         <>
-
+        
             <Container>
                  <Logo/>
             </Container>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,39 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from 'lucide-react'
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon, MoonIcon} from 'lucide-react'
 import styles from './styles.module.css'
+import { useState, useEffect } from 'react'
 
+type AvailableThemes = 'light' | 'dark'
 
 export function Menu(){
-    
-    return <div className= {styles.menu}>
-        <a className={styles.menuLink} href="#">
+
+    const [theme, setTheme] = useState<AvailableThemes>(()=>{
+        const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
+
+        return storageTheme;
+    });
+
+    function handleThemeChange(e:React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+        e.preventDefault();
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    return <nav className= {styles.menu}>
+        <a className={styles.menuLink} href="#" arial-label = 'Ir para a Home' title = 'Ir para a Home' >
             <HouseIcon />
         </a>
-         <a className={styles.menuLink} href="#">
+         <a className={styles.menuLink} href="#" arial-label = 'Ver Histórico' title = 'Ver Histórico' >
             <HistoryIcon />
         </a>
-         <a className={styles.menuLink} href="#">
+         <a className={styles.menuLink} href="#" arial-label = 'Ir para a Home' title = 'Ir para a Home'>
             <SettingsIcon/>
         </a>
-         <a className={styles.menuLink} href="#">
-            <SunIcon/>
+         <a className={styles.menuLink} href="#" arial-label = 'Ir para a Home' title = 'Mudar o tema' onClick ={handleThemeChange} >
+            {theme === 'light' ? <MoonIcon/> : <SunIcon/>}
         </a>
-    </div>
-} 
+    </nav>
+}
